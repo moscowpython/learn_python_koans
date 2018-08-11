1b13440ee284b3938fa8adea943f6f215168b357
melevir <melevir@gmail.com>
2018-08-11 14:45:09 +0300

    allow to run koan set separately

---
+++ koans/about_floats.py
@@ -0,0 +1,9 @@
+from koans_plugs import *
+
+
+def test_can_sum_up():
+    """
+        Вещественные числа можно складывать
+    """
+
+    assert 2.5 + 1.2 == __
--- run.py
+++ run.py
@@ -1,8 +1,16 @@
+import sys
+
 import pytest
 
 
 if __name__ == '__main__':
-    pytest.main([
+    pytest_params = [
         '-x',  # first fail
         '--verbose',  # на каждый тест по строке в выводе
-    ])
+    ]
+
+    args = sys.argv
+    if args and len(args) >= 2:
+        pytest_params.append('koans/about_%s.py' % args[1])
+
+    pytest.main(pytest_params)
