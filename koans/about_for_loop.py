2750cc0ff6aee04ef3024e33f8fa542f285c8fd1
dasha <jrdev88@gmail.com>
2018-08-28 01:32:59 +0300

    edits in for_loop

--- koans/about_for_loop.py
+++ koans/about_for_loop.py
@@ -3,9 +3,9 @@ from koans_plugs import *
 
 def test_for_loop():
     """
-        Цикл for позволяет перебрать поочередно элементы итерируемого объекта
+        Цикл for позволяет перебрать поочередно элементы по заданной последовательности
         и выполнить для каждого из них тело цикла. В общем случае цикл задается так:
-        for имяПеременной in нечтоИтерируемое
+        for имяПеременной in последовательность
 
         Например, для строки 'hello' зададим цикл for, который вернет каждую букву
         данной строки, обратившись к каждому ее символу по его индексу.
@@ -14,23 +14,27 @@ def test_for_loop():
         for letter in str1:
             print(letter)
 
-        Цикл выполнит тело столько раз, сколько итерируемых объектов мы передали в объекте
+        Цикл выполнит тело столько раз, сколько итерируемых объектов мы передали в последовательности
         (количество символов в строке)
 
     """
 
     str1 = "Python"
 
-    for letter in str1:
-        assert letter = ___
+    for letter in str1[2]:
+        assert letter == ___
 
 
 def test_for_loop_with_list:
     """
         Цикл for может проходить не только по строкам, но и по спискам и кортежам
+        Например, для list1 = [2, 4, 6] цикл
+        for integer in list1:
+            print(integer)
+        выведет каждый элемент списка поочередно, аналогично подобному циклу для строки
     """
 
-    list1 = [2, 4, 7, 9]
+    list1 = [5]
 
     for number in list1:
         assert number = ___
@@ -38,22 +42,29 @@ def test_for_loop_with_list:
 
 def test_for_loop_with_range():
     """
-        Часто цикл for используется с функцией range (она генерирует диапазон итерируемых объектов,
-        состоящих из целых чисел, которые записаны последовательно)
+        Часто цикл for используется с функцией range (она генерирует диапазон целых чисел,
+        которые записаны последовательно). При указании одного параметра последовательность
+        будет сгенерирована от 0 до заданного числа, не включая его
     """
+    sum = 0
+    for i in range(3):
+        sum = sum + i
 
-    for i in range(5):
-        assert i = ___
+    assert sum == ___
 
 
 def test_for_loop_with_specified_range():
     """
-        В функцию range можно передать два параметра (первого и последнего итерируемого
-        числа, при этом последнее число в диапазон не включается), разделив их запятой
+        В функцию range можно передать два параметра (первого и последнего числа
+        заданного диапазона, при этом последнее число в последовательность не включается),
+        разделив их запятой
     """
 
-    for i in range(2, 7):
-        assert i = ___
+    sum = 0
+    for i in range(2, 5):
+        sum = sum + i
+
+    assert sum == ___
 
 
 def test_for_loop_with_step_range():
@@ -64,8 +75,11 @@ def test_for_loop_with_step_range():
         Например, range(2, 9, 2) будет состоять из четных чисел.
     """
 
+    sum = 0
     for i in range(3, 12, 3):
-        assert i = ___
+        sum = sum + i
+
+    assert sum == ___
 
 
 def test_for_loop_with_break():
@@ -73,10 +87,13 @@ def test_for_loop_with_break():
         Работа цикла for может быть досрочно прервана командой break
     """
 
-    for i in range(6):
-        if i == 2:
+    sum = 0
+    for i in range(3):
+        if i == 1:
             break
-        assert i * 2 = ___
+        sum = sum + i
+
+    assert sum == ___
 
 def test_for_loop_with_continue():
     """
@@ -84,16 +101,18 @@ def test_for_loop_with_continue():
         тело цикла
     """
 
-for i in 'Python':
-    if i == 0:
-        continue
-    assert i * 2 = ___
+    sum = 0
+    for i in range(3, 7):
+        if i == 5:
+            continue
+        sum = sum + i
 
+    assert sum == ___
 
 def test_for_loop_with_enumerate():
     """
         В python есть встроенная функция enumerte, которая позволяет получить доступ
-        к индексу итерируемых объектов, возвращая при каждом проходе цикла индекс и значение переменной.
+        к индексу последовательности, возвращая при каждом проходе цикла индекс и значение переменной.
 
         Например, необходимо отследить индексы при проходе цикла for по строке 'Hello'.
         str1 = 'Hello'
@@ -103,18 +122,18 @@ def test_for_loop_with_enumerate():
         Результатом работы цикла будет последовательный вывод "0 H", "1 e", "2 l", "3 l", "4 o"
     """
 
-    list1 = ['Hello', ',', 'world', '!']
+    list1 = ['Python']
     for index, element in enumerate(list1):
-        print(index, element)
+        assert index == ___, element == ____
 
 
 def test_for_loop_with_enumerate_start():
     """
-        По умолчанию нумерация индексов итерируемых объектов начинается с 0, то есть
-        enumerate(iter) можно переписать как enumerate(iter, start = 0). Можно поменять
+        По умолчанию нумерация индексов последовательности начинается с 0, то есть
+        enumerate(iter) можно переписать как enumerate(iter, start=0). Можно поменять
         начальное значение индексов при помощи переменной start
     """
 
-    str1 = 'Python'
-    for index, letter in enumerate(str1, start = 1):
-        print(index, element)
+    list1 = ['Python']
+    for index, element in enumerate(list1, start=1):
+        assert index == ___, element == ____
