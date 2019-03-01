12846e0e37003e9d8bd36b740e016b69dd0081a7
Nikolai Kiselev <31371501+nshrd@users.noreply.github.com>
2019-03-01 14:53:33 +0300

    fixed errors in 3 tests

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -8,24 +8,30 @@ def test_create_list_with_literal():
         Список может содержать коллекции произвольных типов
     """
 
-    my_list = ___ # попробуйте ввести такие варианты: list(), ['Hello', 'world'], {'Hello'}
+    my_list = ['Hello', 'world'] 
     
-    assert isinstance(my_list, list)
+    assert my_list == ___ # попробуйте ввести такие варианты: list(), ['Hello', 'world'], {'Hello'}
 
 
 def test_create_list_with_constructor():
     """
-        Список в Python также можно создать с помощью конструктора списка
+        Список в Python также можно создать с помощью конструктора списка list()
+        В конструктор можно передавать любой последовательный объект
+        Например, передаем в конструктор list() строку 'Learn'
+        your_list = list('Learn') и получаем список your_list = ['L', 'e', 'a', 'r', 'n'] 
     """
 
-    my_list = ___ # попробуйте ввести такие варианты: list('Hello world!'), list({'a': 1, 'b': 2})
+    my_list = list('Hello, world!') 
 
-    assert isinstance(my_list, list)
+    assert my_list == ___('Hello, world!') # попробуйте такие варианты: dict, list, set
 
 
 def test_list_index():
     """
         Можно обращаться к отдельным элементам списка
+        Отсчет элементов в списке начинается с 0
+        Например, в списке your_city = ['Moscow', 'Kiev', 'London']
+        обращаемся к первому элементу your_city[0] = 'Moscow'
     """
 
     my_list = ['Hello', 'world', '!']
