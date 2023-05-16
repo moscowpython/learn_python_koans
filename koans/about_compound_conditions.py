148e87a0b981c90cb86a87de52d5ecf2a53003dc
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-16 17:58:15 +0300

    compoun_conditions exercise

--- koans/about_compound_conditions.py
+++ koans/about_compound_conditions.py
@@ -3,68 +3,68 @@ from koans_plugs import *
 
 def test_and_returns_one_of_the_operands():
     """
-        Оператор and выполняет булевы операции, но возвращает не булево 
-        значение, а значение одного из операндов.
+    Оператор and выполняет булевы операции, но возвращает не булево
+    значение, а значение одного из операндов.
     """
-    r = 'a' and 'b'
-    assert r == _____  # попробуйте такие варианты: True, False, 'a', 'b'
+    r = "a" and "b"
+    assert r == "b"  # попробуйте такие варианты: True, False, 'a', 'b'
 
 
 def test_or_returns_one_of_the_operands():
     """
-        Оператор or выполняет булевы операции, но возвращает не булево
-         значение, а значение одного из операндов.
+    Оператор or выполняет булевы операции, но возвращает не булево
+     значение, а значение одного из операндов.
     """
-    r = 'a' or 'b'
-    assert r == _____  # попробуйте такие варианты: True, False, 'a', 'b'
+    r = "a" or "b"
+    assert r == "a"  # попробуйте такие варианты: True, False, 'a', 'b'
 
 
 def test_and_returns_first_false_operand():
     """
-        Если какой-то из операндов оператора and является ложью, 
-        результатом будет первое такое значение.
-        Ложью в Python являются 0, '', [], (), {} и None
+    Если какой-то из операндов оператора and является ложью,
+    результатом будет первое такое значение.
+    Ложью в Python являются 0, '', [], (), {} и None
     """
-    r = 'a' and '' and []
-    assert r == _____  # попробуйте такие варианты: True, False, 'a', '', []
+    r = "a" and "" and []
+    assert r == ""  # попробуйте такие варианты: True, False, 'a', '', []
 
 
 def test_or_returns_last_false_operand_if_all_operands_are_false():
     """
-        Если все значения оператора or являются ложью, or возвращает 
-        последнее такое значение.
+    Если все значения оператора or являются ложью, or возвращает
+    последнее такое значение.
     """
-    r = '' or [] or {}
-    assert r == _____  # попробуйте такие варианты: True, False, '',  [], {}
+    r = "" or [] or {}
+    assert r == {}  # попробуйте такие варианты: True, False, '',  [], {}
 
 
 def test_python_supports_not_operator():
     """
-        В Python есть логический оператор not 
+    В Python есть логический оператор not
     """
     y = False
-    assert not y == _____  # попробуйте такие варианты: True, False
+    assert not y == True  # попробуйте такие варианты: True, False
 
 
 def test_and_has_higher_priority_then_or():
     """
-        Оператор and имеет выше приоритет, чем or 
+    Оператор and имеет выше приоритет, чем or
     """
-    r = 'a' and 'b' or []
-    assert r == _____  # попробуйте такие варианты: 'a', 'b', [], True, False
+    r = "a" and "b" or []
+    assert r == "b"  # попробуйте такие варианты: 'a', 'b', [], True, False
 
 
 def test_not_has_higher_priority_then_and():
     """
-        Оператор and имеет выше приоритет, чем or 
+    Оператор and имеет выше приоритет, чем or
     """
-    r = not 'a' and 'b' or []
-    assert r == _____  # попробуйте такие варианты: 'a', 'b', [], True, False
+    r = not "a" and "b" or []
+    assert r == []  # попробуйте такие варианты: 'a', 'b', [], True, False
 
 
 def test_brackets_can_change_priority():
     """
-        Оператор and имеет выше приоритет, чем or 
+    Оператор and имеет выше приоритет, чем or
     """
-    r = '' and ([] or 'a')
-    assert r == _____  # попробуйте такие варианты: 'a', '', [], True, False
+    r = "" and ([] or "a")
+    assert r == ""  # попробуйте такие варианты: 'a', '', [], True, False
