a993ac6736e025687b3b99ef2c917eeee5bca92c
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-16 17:36:01 +0300

    bool exercise

--- koans/about_bool.py
+++ koans/about_bool.py
@@ -3,7 +3,7 @@ from koans_plugs import *
 
 def test_has_true_literal():
     """
-        У логического типа есть литерал, обозначающий истину
+    У логического типа есть литерал, обозначающий истину
     """
     a = True  # попробуйте такие варианты: TRUE, true, True
     assert a
@@ -11,7 +11,7 @@ def test_has_true_literal():
 
 def test_has_false_literal():
     """
-        У логического типа есть литерал, обозначающий ложь
+    У логического типа есть литерал, обозначающий ложь
     """
     a = False  # попробуйте такие варианты: FALSE, false, False
     assert not a
@@ -19,52 +19,52 @@ def test_has_false_literal():
 
 def test_python_can_calculate_bool_expressions():
     """
-        Python может проверять, является выражение истиной или ложью
+    Python может проверять, является выражение истиной или ложью
     """
     assert (3 > 2) == True  # "3 > 2" – это верно (True) или ложно (False)?
 
 
 def test_can_assign_bool_expressions_to_variable():
     """
-        Логические выражения можно записывать в переменную.
+    Логические выражения можно записывать в переменную.
 
-        Тогда в этой переменной окажется True или False в зависимости от того,
-        истинно выражение или ложно.
+    Тогда в этой переменной окажется True или False в зависимости от того,
+    истинно выражение или ложно.
     """
     a = 3 < 2
-    assert a == _____
+    assert a == False
 
 
 def test_assert_accepts_bool():
     """
-        Конструкция assert требует указания bool следом за словом assert.
+    Конструкция assert требует указания bool следом за словом assert.
 
-        Если в bool записана истина, то всё работает.
+    Если в bool записана истина, то всё работает.
     """
-    a = 3 < __  # укажите любое число, чтобы в a было True
+    a = 3 < 7  # укажите любое число, чтобы в a было True
     assert a
 
 
 def test_can_use_not():
     """
-        not превращает True в False, а False в True.
+    not превращает True в False, а False в True.
     """
     a = True
-    assert not a == _____
+    assert not a == False
 
 
 def test_can_use_equality_check():
     """
-        == возвращает True, если слева находится такое же значение, что и справа.
+    == возвращает True, если слева находится такое же значение, что и справа.
 
-        Иначе возвращает False.
+    Иначе возвращает False.
     """
-    assert 3 + 2 == 1 + __
+    assert 3 + 2 == 1 + 4
 
 
 def test_can_assign_equality_check_to_variable():
     """
-        Результат сравнения можно записывать в переменную.
+    Результат сравнения можно записывать в переменную.
     """
     a = 3 + 2 == 1 + 4
-    assert a == __
+    assert a == True
