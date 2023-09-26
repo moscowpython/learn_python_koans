87890669edac063de2a12c3d56fdbaadd23d7eef
Igor Dikarev <igor.dikarev@gmail.com>
2023-09-26 14:21:30 +0300

    solve koans

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -3,110 +3,122 @@ from koans_plugs import *
 
 def test_create_list_with_literal():
     """
-        Список в Python можно создать с помощью литерала списка 
-        Литерал списка это квадратные скобки: []
-        Список может содержать коллекции произвольных типов
+    Список в Python можно создать с помощью литерала списка
+    Литерал списка это квадратные скобки: []
+    Список может содержать коллекции произвольных типов
     """
 
-    my_list = ['Hello', 'world'] 
-    
-    assert my_list == ___ # попробуйте ввести такие варианты: list(), ['Hello', 'world'], {'Hello'}
+    my_list = ["Hello", "world"]
+
+    assert my_list == [
+        "Hello",
+        "world",
+    ]  # попробуйте ввести такие варианты: list(), ['Hello', 'world'], {'Hello'}
 
 
 def test_create_list_with_constructor():
     """
-        Список в Python также можно создать с помощью функции list()
-        В функцию можно передавать любой последовательный объект
-        Например, передаем в функцию list() строку 'Learn'
-        your_list = list('Learn') и получаем список your_list = ['L', 'e', 'a', 'r', 'n'] 
+    Список в Python также можно создать с помощью функции list()
+    В функцию можно передавать любой последовательный объект
+    Например, передаем в функцию list() строку 'Learn'
+    your_list = list('Learn') и получаем список your_list = ['L', 'e', 'a', 'r', 'n']
     """
 
-    my_list = list('Hello, world!') 
+    my_list = list("Hello, world!")
 
-    assert my_list == ___('Hello, world!') # попробуйте ввести такие варианты: dict, list, set
+    assert my_list == list(
+        "Hello, world!"
+    )  # попробуйте ввести такие варианты: dict, list, set
 
 
 def test_list_index():
     """
-        Можно обращаться к отдельным элементам списка
-        Отсчет элементов в списке начинается с 0
-        Например, в списке your_city = ['Moscow', 'Kiev', 'London']
-        обращаемся к первому элементу your_city[0] = 'Moscow'
+    Можно обращаться к отдельным элементам списка
+    Отсчет элементов в списке начинается с 0
+    Например, в списке your_city = ['Moscow', 'Kiev', 'London']
+    обращаемся к первому элементу your_city[0] = 'Moscow'
     """
 
-    my_list = ['Hello', 'world', '!']
+    my_list = ["Hello", "world", "!"]
 
-    assert my_list[2] == ___
+    assert my_list[2] == "!"
 
 
 def test_add_item_to_list():
     """
-        Добавить элемент в список можно с помощью метода списка append
-        Например, в существующий список your_list = ['Moscow', 'Kiev']
-        можно добавить элемент your_list.append('London').
-        Новые элементы становятся в конец списка your_list = ['Moscow', 'Kiev', 'London']
+    Добавить элемент в список можно с помощью метода списка append
+    Например, в существующий список your_list = ['Moscow', 'Kiev']
+    можно добавить элемент your_list.append('London').
+    Новые элементы становятся в конец списка your_list = ['Moscow', 'Kiev', 'London']
     """
 
-    my_list = ['Hello', 'world']
-    my_list.append('!')
+    my_list = ["Hello", "world"]
+    my_list.append("!")
 
-    assert my_list == ___ # попробуйте ввести такие варианты: ['Hello world!'], ['Hello', 'world!'], ['Hello', 'world', '!']
+    assert my_list == [
+        "Hello",
+        "world",
+        "!",
+    ]  # попробуйте ввести такие варианты: ['Hello world!'], ['Hello', 'world!'], ['Hello', 'world', '!']
 
 
 def test_operator_len():
     """
-        Списки имеют длину. Посчитать длину можно с помощью функции len()
-        Например, your_list = [1, 2, 3]
-        len(your_list) == 3
+    Списки имеют длину. Посчитать длину можно с помощью функции len()
+    Например, your_list = [1, 2, 3]
+    len(your_list) == 3
     """
 
-    my_list = ['Learn', 'Python']
+    my_list = ["Learn", "Python"]
 
-    list_len = ___ # попробуйте ввести такие варианты: 2, 3, 4
+    list_len = 2  # попробуйте ввести такие варианты: 2, 3, 4
 
     assert len(my_list) == list_len
 
 
 def test_remove_from_list():
     """
-        Из списка можно удалять элементы. Для этого используется метод списка remove 
-        Например, есть список your_list = ['Moscow', 'Kiev', 'London'] и мы хотим
-        удалить элемент 'London'. Для этого передаем название элемента в метод
-        your_list.remove('London') и получаем your_list = ['Moscow', 'Kiev']
+    Из списка можно удалять элементы. Для этого используется метод списка remove
+    Например, есть список your_list = ['Moscow', 'Kiev', 'London'] и мы хотим
+    удалить элемент 'London'. Для этого передаем название элемента в метод
+    your_list.remove('London') и получаем your_list = ['Moscow', 'Kiev']
     """
 
-    my_list = ['Learn', 'Python', '!']
-    my_list.remove('!')
+    my_list = ["Learn", "Python", "!"]
+    my_list.remove("!")
 
-    assert my_list == ___
+    assert my_list == ["Learn", "Python"]
 
 
 def test_lists_can_sum_up():
-    """ 
-        Cписки можно складывать. 
-        Например, есть два списка learn_ = [1, 2] и python_ = [3, 4]
-        Складываем их и получаем learn_ + python_ == [1, 2, 3, 4]
+    """
+    Cписки можно складывать.
+    Например, есть два списка learn_ = [1, 2] и python_ = [3, 4]
+    Складываем их и получаем learn_ + python_ == [1, 2, 3, 4]
     """
 
-    my_list = ['learn']
-    your_list = ['python']
-    our_list = ___ # попробуйте ввести такие варианты: ['learnpython'], ['learn', 'python'], [['learn'], ['python']]
+    my_list = ["learn"]
+    your_list = ["python"]
+    our_list = [
+        "learn",
+        "python",
+    ]  # попробуйте ввести такие варианты: ['learnpython'], ['learn', 'python'], [['learn'], ['python']]
 
     assert our_list == my_list + your_list
 
 
 def test_for_lists_loop():
     """
-        Так как список является последовательным обьектом, то его можно обходить с помощью цикла for
-        Например, есть список your_list = [1, 2, 3]. Цикл пройдется по каждому элементу в списке
-        for element in your_list:
-            print(element)
-        На каждом обходе списка мы выведем на экран текущее значение element:
-        1
-        2
-        3
+    Так как список является последовательным обьектом, то его можно обходить с помощью цикла for
+    Например, есть список your_list = [1, 2, 3]. Цикл пройдется по каждому элементу в списке
+    for element in your_list:
+        print(element)
+    На каждом обходе списка мы выведем на экран текущее значение element:
+    1
+    2
+    3
     """
 
-    my_list = ['Python']
+    my_list = ["Python"]
     for element in my_list:
-        assert element == ___
+        assert element == "Python"
--- koans/about_strings.py
+++ koans/about_strings.py
@@ -3,162 +3,161 @@ from koans_plugs import *
 
 def test_string_indexes():
     """
-        В строке можно обращаться к отдельным символам
+    В строке можно обращаться к отдельным символам
     """
-    str1 = 'Hello, world!'
+    str1 = "Hello, world!"
 
-    assert str1[5] == __
+    assert str1[5] == ","
 
 
 def test_string_slice():
     """
-        Из строки можно брать срез
+    Из строки можно брать срез
     """
-    str1 = 'Hello, world!'
+    str1 = "Hello, world!"
 
-    assert str1[5:9] == __
+    assert str1[5:9] == ", wo"
 
 
 def test_string_for():
     """
-        По строке можно итерироваться, как по списку
+    По строке можно итерироваться, как по списку
     """
-    str1 = 'Hello, world!'
+    str1 = "Hello, world!"
 
     symbol_count = 0
     for symbol in str1:
         symbol_count += 1
 
-    assert symbol_count == __
+    assert symbol_count == 13
 
 
 def test_string_add():
     """
-        Строки можно складывать
+    Строки можно складывать
     """
-    str1 = 'Hello, ' + 'world!'
+    str1 = "Hello, " + "world!"
 
-    assert str1 == __
+    assert str1 == "Hello, world!"
 
 
 def test_string_add_extra():
     """
-        Можно добавлять строки к уже существующей
+    Можно добавлять строки к уже существующей
     """
-    str1 = 'Hello, '
-    str1 += 'world!'
+    str1 = "Hello, "
+    str1 += "world!"
 
-    assert str1 == __
+    assert str1 == "Hello, world!"
 
 
 def test_string_convert_int():
     """
-        Числа можно преобразовывать в строку
+    Числа можно преобразовывать в строку
     """
     str1 = str(123)
 
-    assert str1 == __
+    assert str1 == "123"
 
 
 def test_string_add_int():
     """
-        Строки нельзя складывать c числами без преобразования числа в строку
+    Строки нельзя складывать c числами без преобразования числа в строку
     """
-    str1 = 'Hello, ' + str(123)
+    str1 = "Hello, " + str(123)
 
-    assert str1 == __
+    assert str1 == "Hello, 123"
 
 
 def test_string_len():
     """
-        Длину строки можно померить функцией len
+    Длину строки можно померить функцией len
     """
-    str1 = len('Hello, ')
+    str1 = len("Hello, ")
 
-    assert str1 == __
+    assert str1 == 7
 
 
 def test_string_split():
     """
-        Строку можно разбить другой строкой на список строк
+    Строку можно разбить другой строкой на список строк
     """
-    str1 = 'Hello, world! Hello, everyone!'
-    splited_str = str1.split(' ')
+    str1 = "Hello, world! Hello, everyone!"
+    splited_str = str1.split(" ")
 
-    assert [__, __, __, __] == splited_str
+    assert ["Hello,", "world!", "Hello,", "everyone!"] == splited_str
 
 
 def test_string_join():
     """
-        Список строк можно объединить в одну строку
+    Список строк можно объединить в одну строку
     """
-    str1 = '! '.join(['Hello', 'world', 'Hello', 'everyone'])
+    str1 = "! ".join(["Hello", "world", "Hello", "everyone"])
 
-    assert str1 == __
+    assert str1 == "Hello! world! Hello! everyone"
 
 
 def test_string_methods():
     """
-        У строки есть методы для приведения некоторых ее символов в разные регистры
+    У строки есть методы для приведения некоторых ее символов в разные регистры
     """
 
-    assert __ == 'hello'.capitalize()
-    assert __ == 'hello'.upper()
-    assert __ == 'Hello World'.lower()
-    assert __ == 'hello world'.title()
-    assert __ == 'HeLlo wORld'.swapcase()
+    assert "Hello" == "hello".capitalize()
+    assert "HELLO" == "hello".upper()
+    assert "hello world" == "Hello World".lower()
+    assert "Hello World" == "hello world".title()
+    assert "hElLO WorLD" == "HeLlo wORld".swapcase()
 
 
 def test_string_in():
     """
-        В строке легко можно проверить наличие подстроки
+    В строке легко можно проверить наличие подстроки
     """
 
-    str1 = 'Hello, world!'
-    find_in_str1 = 'world' in str1
+    str1 = "Hello, world!"
+    find_in_str1 = "world" in str1
 
-
-    assert find_in_str1 == __
+    assert find_in_str1 == True
 
 
 def test_string_format():
     """
-        В строку можно подставлять объекты различных типов с помощью форматирования
+    В строку можно подставлять объекты различных типов с помощью форматирования
     """
 
-    str1 = 'Hello, {}! {}'
-    str2 = str1.format(123, 'Hello')
+    str1 = "Hello, {}! {}"
+    str2 = str1.format(123, "Hello")
 
-    assert str2 == __
+    assert str2 == "Hello, 123! Hello"
 
 
 def test_string_empty():
     """
-        Пустая строка может использоваться при вычислении логических выражений
+    Пустая строка может использоваться при вычислении логических выражений
     """
-    str1 = ''
-    str2 = ''
-    str3 = 'hello'
+    str1 = ""
+    str2 = ""
+    str3 = "hello"
 
     empty_string = not str1
-    assert empty_string == __
+    assert empty_string == True
 
     empty_string = str1 or str2 or not str3
-    assert empty_string == __
+    assert empty_string == False
 
 
 def test_string_compare():
     """
-        Строки можно сравнивать, сравнение производится в лексикографическом порядке.
+    Строки можно сравнивать, сравнение производится в лексикографическом порядке.
     """
-    str1 = 'Hello, world!'
-    str2 = 'Hello, ' + 'world!'
+    str1 = "Hello, world!"
+    str2 = "Hello, " + "world!"
 
     string_compare = str1 == str2
-    assert string_compare == __
+    assert string_compare == True
 
-    string_compare = 'AAA' < 'AAB'
-    assert string_compare == __
+    string_compare = "AAA" < "AAB"
+    assert string_compare == True
 
-    string_compare = 'aAA' < 'AAB'
-    assert string_compare == __
+    string_compare = "aAA" < "AAB"
+    assert string_compare == False
